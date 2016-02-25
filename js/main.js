3d2d43da79310aea439ecaea5b0564dc75e6892f
Frank Raterink <frank@q42.nl>
2016-02-25 10:18:29 +0100

    removed wobbling menu

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ function Franklabs() {
 
 Franklabs.prototype = {
 	init: function() {
-		
+
 		this.$body = $('body');
 		this.$window = $(window);
 		this.$header = $('.site-header');
@@ -26,18 +26,16 @@ Franklabs.prototype = {
 		// set this for event handlers
 		// to prevent more than one instance of mousemove and scroll
 		// and to be able to unbind
-		this.onMouseMove = this.onMouseMove.bind(this);
 		this.onScroll = this.onScroll.bind(this);
 
 		this.enhanceForNonTouchDevices();
 
 		this.$window.resize(this.enhanceForNonTouchDevices.bind(this));
 	},
-	
+
 
 	enhanceForNonTouchDevices: function () {
 		// unbind previous bind
-		this.unbindHeaderAnimation();
 
 		if (this.is_touch_device() || innerWidth < 1150) {
 			this.$body.removeClass('enhanceUIforNonTouch');
@@ -45,7 +43,7 @@ Franklabs.prototype = {
 			this.$header.removeClass('hidden');
 		} else {
 			this.$body.addClass('enhanceUIforNonTouch');
-			this.bindHeaderAnimation();	
+
 		}
 	},
 
@@ -81,7 +79,7 @@ Franklabs.prototype = {
 		});
 
 		$("html, body").animate({ scrollTop: 0 }, 800, function(){
-			
+
 			var photoFrames = ui.select('.photo-frame'); // select returns Iterator
 			photoFrames.stagger('start', {
 				interval: 5,
@@ -107,7 +105,7 @@ Franklabs.prototype = {
 				e.preventDefault();
 				if (this.gotoPhoto < $photos.length - 1) {
 					this.gotoPhoto++;
-					this.$window.scrollTop($photos.eq(this.gotoPhoto).offset().top);	
+					this.$window.scrollTop($photos.eq(this.gotoPhoto).offset().top);
 				}
 			}
 
@@ -116,50 +114,14 @@ Franklabs.prototype = {
 				if (this.gotoPhoto > 0) {
 					this.gotoPhoto--;
 					this.$window.scrollTop($photos.eq(this.gotoPhoto).offset().top);
-				}			
+				}
 			}
 		}.bind(this));
-	},
-
-	bindHeaderAnimation: function() {
-		this.$body.on('mousemove', this.onMouseMove);
-		this.$window.on('scroll', this.onScroll);
-	},
-
-	unbindHeaderAnimation: function() {
-		this.$body.off('mousemove', this.onMouseMove);
-		this.$window.off('scroll', this.onScroll);
-	},
-
-	onMouseMove: function() {
-		this.$header.addClass('visible');
-
-		if (this.headerAnimation) {
-			this.setHeaderLocation(event.pageX, event.pageY);	
-		}
 	},
 
 	onScroll: function() {
 		this.windowScrollTop = this.$window.scrollTop();
 	},
-
-	setHeaderLocation: function(x, y) {
-		var movedX = (x * -1 + 300)/1.5;
-		var movedY = (y * -1 + 300)/1;
-		if (this.windowScrollTop > 0) {
-			movedY += this.windowScrollTop;
-		}
-		this.setTranslate(movedX, movedY);
-	},
-
-	setTranslate: function(x, y) {
-		var translate = 'translate3d(' + x + 'px, ' + y + 'px, 0px)';
-		this.$header.css({
-			'-webkit-transform' : translate,
-			'-ms-transform'     : translate,
-			'transform'         : translate
-		});
-	}
 
 }
 
